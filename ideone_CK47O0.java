42d86da69f231186e81c8cd5601f1a26323224b0
Abhishek131095 <amohstijita@gmail.com>
2017-07-09 13:47:12 +0530

    Add files via upload

---
+++ ideone_CK47O0.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class guvi
+{
+ public static void main(String[ ] arg)
+    {
+       int a=2,b=3,c=4;
+    if(a>b & a>c)
+  {
+  System.out.println("a is largest");
+}
+else if(b>a & b>c)
+{
+System.out.println("b is largest");
+}
+else
+{
+System.out.println("c is largest");
+}
+}
+}
