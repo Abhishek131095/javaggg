f196cfc251210bbbe4a0b4e62a02f81bda980d9b
Abhishek131095 <amohstijita@gmail.com>
2017-07-09 11:59:22 +0530

    Add files via upload

---
+++ ideone_vBHqWK (1).java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class guvi
+{
+    public static void main(String[] args) 
+    {
+        int n=-5;
+        if(n > 0)
+        {
+            System.out.println("The given number "+n+" is Positive");
+        }
+        else if(n < 0)
+        {
+            System.out.println("The given number "+n+" is Negative");
+        }
+        else
+        {
+            System.out.println("The given number "+n+" is neither Positive nor Negative ");
+        }
+    }
+}
