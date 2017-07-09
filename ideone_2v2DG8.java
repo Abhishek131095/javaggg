c39b52d3a3e516634dbfe8d5ef3b7e08f238642f
Abhishek131095 <amohstijita@gmail.com>
2017-07-09 13:54:51 +0530

    Add files via upload

---
+++ ideone_2v2DG8.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class guvi
+{
+ public static void main(String[ ] arg)
+    {
+       int a=1996;
+    if(a%4==0)
+  {
+  System.out.println(a+" is leap year");
+}
+else 
+System.out.println(a+" is not leap year");
+}
+}
