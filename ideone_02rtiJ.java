56b235c28206953e4f23679ece4dd882bf88bf40
Abhishek131095 <amohstijita@gmail.com>
2017-07-09 11:48:21 +0530

    Add files via upload

---
+++ ideone_02rtiJ.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class guvi
+{
+    public static void main(String[] args) 
+    {
+        int n=4;
+        if(n > 0)
+        {
+            System.out.println("The given number "+n+" is Positive");
+        }
+        else if(n < 0)
+        {
+            System.out.println("The given number "+n+" is Negative");
+        }
+        else
+        {
+            System.out.println("The given number "+n+" is neither Positive nor Negative ");
+        }
+    }
+}
+
