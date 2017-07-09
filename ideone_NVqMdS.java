98644863b811aa901cad6f7e7aa0838304da66d6
Abhishek131095 <amohstijita@gmail.com>
2017-07-09 14:47:44 +0530

    Add files via upload

---
+++ ideone_NVqMdS.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class guvi
+{
+	public static void main(String[] args)
+	{
+		int sum = 0; 
+for (int i = 1; i <= 100; i++)
+sum=sum+ i; 
+System.out.println("The sum is " + sum);
+	}
+}
