d465abd89e6aac8cef3c8bd78663ebe26bcc0466
Abhishek131095 <amohstijita@gmail.com>
2017-07-09 13:11:10 +0530

    Add files via upload

---
+++ ideone_7Vue1L.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class guvi
+{
+    public static void main(String[] args) 
+    {
+        int n=4;
+        System.out.println("Enter the number you want to check:");
+        if(n%2==0)
+        {
+            System.out.println("The given number "+n+" is even");
+        }
+        else
+        {
+            System.out.println("The given number "+n+" is odd ");
+        }
+    }
+}
